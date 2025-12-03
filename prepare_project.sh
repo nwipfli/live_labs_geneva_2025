952424b66f280bce19734a5051ce0710b8c4fb0b
Nicolas Wipfli <nwipfli@google.com>
2025-12-03 13:52:59 +0100

    Working version

--- prepare_project.sh
+++ prepare_project.sh
@@ -7,9 +7,6 @@ REGION="us-central1"
 TEMPLATE_DISPLAY_NAME="standard-runtime-template"
 NETWORK="default"
 
-echo "------------------------------------------------"
-echo "Processing Project: $PROJECT_ID"
-
 # Enable the Vertex AI API (Required if not already enabled)
 echo "Enabling APIs"
 gcloud services enable \
@@ -18,6 +15,12 @@ gcloud services enable \
     bigqueryconnection.googleapis.com \
     compute.googleapis.com \
     --project="$PROJECT_ID"
+
+if [ $? -eq 0 ]; then
+    echo "SUCCESS: APIs enabled for $PROJECT_ID. Waiting 20 sec."
+else
+    echo "ERROR: Failed to enable the APIs for $PROJECT_ID"
+fi
 
 sleep 20
 
@@ -48,5 +51,25 @@ else
     echo "ERROR: Failed to create the runtime for $PROJECT_ID"
 fi
 
-
+# Create the bucket
     
+gcloud storage buckets create gs://"notebooks-$PROJECT_ID" \
+    --project="$PROJECT_ID" \
+    --default-storage-class="STANDARD" \
+    --location="$REGION"
+
+if [ $? -eq 0 ]; then
+    echo "SUCCESS: Bucket created for $PROJECT_ID"
+else
+    echo "ERROR: Failed to create the bucket for $PROJECT_ID"
+fi
+
+gcloud storage cp ./Notebooks/* gs://"notebooks-$PROJECT_ID"
+
+if [ $? -eq 0 ]; then
+    echo "SUCCESS: Notebooks imported for $PROJECT_ID"
+else
+    echo "ERROR: Failed to import the notebooks for $PROJECT_ID"
+fi
+
+echo "Environment ready !"
